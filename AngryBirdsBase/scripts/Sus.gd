4aa41f2ef367d58c5bfd3a89e873d003f2d5ffe0
Spencer Smith <spencersmith24@outlook.com>
2024-09-13 17:50:00 -0400

    i dont even know what i did here

--- AngryBirdsBase/scripts/Sus.gd
+++ AngryBirdsBase/scripts/Sus.gd
@@ -2,16 +2,6 @@ extends RigidBody2D
 
 @export var particle_resource: Resource
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_body_entered(body):
 	
 	# pop bird
@@ -25,8 +15,3 @@ func _on_body_entered(body):
 	
 	# remove enemy
 	queue_free()
-
-
-func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	#print("OK")
-	pass # Replace with function body.
