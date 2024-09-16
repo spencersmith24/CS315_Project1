29343476bbbc180e312fa7fb8f661c8c683b7712
Spencer Smith <spencersmith24@outlook.com>
2024-09-16 14:13:32 -0400

    once a fish is flung he cannot be flung again

--- AngryBirdsBase/scripts/Birdscript.gd
+++ AngryBirdsBase/scripts/Birdscript.gd
@@ -3,15 +3,17 @@ extends RigidBody2D
 var tracking = false
 var startPos = Vector2()
 const FORCE = 10
+var flingable = true
 
 func _input(evt):
-	if( evt is InputEventMouseButton and evt.button_index == MOUSE_BUTTON_LEFT):
+	if(evt is InputEventMouseButton and evt.button_index == MOUSE_BUTTON_LEFT and flingable):
 		if(evt.is_pressed()):
 			tracking = true
 			startPos = evt.position
 			$DespawnTimer.start()
 		else:
 			tracking = false
+			flingable = false
 			
 			var kickDir = startPos - evt.position
 			gravity_scale = 1
