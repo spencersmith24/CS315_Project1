fcc7364c0ad15cd06074c7aa1b7356b04a3772cb
Spencer Smith <spencersmith24@outlook.com>
2024-09-16 14:39:27 -0400

    create level 2 and 3 buttons

--- AngryBirdsBase/scripts/UI.gd
+++ AngryBirdsBase/scripts/UI.gd
@@ -1,19 +1,30 @@
 extends Control
 
 @export var fish_resource: Resource
+
+@onready var fish_node = $"../Fishies"
+@onready var current_level = get_tree().get_current_scene().name
+
 var score = 0
 var num_enemies
-@onready var fish_node = $"../Fishies"
 
 func add_score():
 	
 	score += 1
 	$Label.text = str(score)
 	
-	#if score is at max - return to main page
+	#if score is at max - show two buttons: menu and lvl2
 	if (score == num_enemies):
 		$menuBtn.visible = true
 		$menuBtn.disabled = false
+		print(current_level)
+		
+		if(current_level == "level1"):
+			$lvl2Btn.visible = true
+			$lvl2Btn.disabled = false
+		elif(current_level == "level2"):
+			$lvl3Btn.visible = true
+			$lvl3Btn.disabled = false
 	else:
 	#if not at max - spawn a new fish
 	
@@ -30,3 +41,11 @@ func _ready():
 
 func _on_menu_btn_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+
+func _on_lvl_2_btn_pressed():
+	get_tree().change_scene_to_file("res://scenes/level2.tscn")
+
+
+func _on_lvl_3_btn_pressed():
+	get_tree().change_scene_to_file("res://scenes/level3.tscn")
